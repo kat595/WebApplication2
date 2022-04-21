c4e8b01f49280803c0212004601bf1f7c066213e
Patryk Murawski <297077@o365.stud.umk.pl>
2022-04-21 14:18:32 +0200

    change properties to nullable in CreateMatchDto

--- WebApplication2/Models/CreateDtos/CreateMatchDto.cs
+++ WebApplication2/Models/CreateDtos/CreateMatchDto.cs
@@ -7,8 +7,8 @@ namespace WebApplication2.Models
         public int Gameweek { get; set; }
         public int HomeId { get; set; }
         public int AwayId { get; set; }
-        public int Result { get; set; }
-        public int Goal_home { get; set; }
-        public int Goal_away { get; set; }
+        public int? Result { get; set; }
+        public int? Goal_home { get; set; }
+        public int? Goal_away { get; set; }
     }
 }
