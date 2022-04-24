f21f4e0c63e58b7bfa94c3475df94bff333ea4a1
Patryk Murawski <297077@o365.stud.umk.pl>
2022-04-24 13:17:45 +0200

    add id property to GetLeagueDto

--- WebApplication2/Models/GetDtos/GetLeagueDto.cs
+++ WebApplication2/Models/GetDtos/GetLeagueDto.cs
@@ -2,6 +2,7 @@
 {
     public class GetLeagueDto
     {
+        public int Id { get; set; }
         public string League_name { get; set; }
         public DateTime Creation_date { get; set; }
         public virtual List<GetUserDto> Users { get; set; }
